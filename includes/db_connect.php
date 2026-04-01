7aedb70507bc9880b6b9ac5d1acd231828410a6d
Sharad Bista <bishalbista737@gmail.com>
2026-04-01 13:25:46 +0545

    feat: add host, user, password, dbname and port_no in for cloud database servive

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -3,12 +3,13 @@ $host = getenv("DB_HOST");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASS");
 $db   = getenv("DB_NAME");
+$port = getenv("DB_PORT") ?: 3306;
 
 if (!$host || !$user || !$db) {
     die("Database environment variables not set properly.");
 }
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
